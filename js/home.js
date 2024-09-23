03a9bceacdf2f14a36389b3476e603c3af397f55
Joy Chowdhury <imjoychy69@gmail.com>
2024-09-24 01:11:02 +0600

    Basic functionality implemented

--- js/home.js
+++ js/home.js
@@ -4,19 +4,104 @@ document.getElementById("blog-button").addEventListener("click", function () {
   window.location.href = "blog.html";
 });
 
-document.getElementById("donate-for-noakhali")
-        .addEventListener('click', function(){
-            const donateAmountForNoakhali = getInputFeldValueById('input-money-for-noakhali');
-            const donationAmount = parseFloat(donateAmountForNoakhali);
-            console.log(typeof donationAmount)
-        })
+document
+  .getElementById("donate-for-noakhali")
+  .addEventListener("click", function () {
+    const accountBalance = document.getElementById("account-balance").innerText;
+    let balance = parseFloat(accountBalance);
 
-// document.getElementById("donate-for-noakhali")
-//         .addEventListener('click', function(){
+    const noakhaliTotalDonationAmount = document.getElementById(
+      "noakhali-total-donation-amount"
+    ).innerText;
+    let noakhaliTotalDonation = parseFloat(noakhaliTotalDonationAmount);
 
-//         })
+    const donatingAmountForNoakhali = getInputFieldValueById(
+      "input-money-for-noakhali"
+    );
 
-// document.getElementById("donate-for-noakhali")
-//         .addEventListener('click', function(){
+    if(!isNaN(donatingAmountForNoakhali)){
+      const donationAmount = parseFloat(donatingAmountForNoakhali);
+   
+    if (donationAmount > balance ) {
+      alert("You have insufficient balance");
+    } else {
+      const updatedBalance = balance - donationAmount;
+      document.getElementById("account-balance").innerText = updatedBalance;
+      const raisedDonationForNoakhali = noakhaliTotalDonation + donationAmount;
+      document.getElementById("noakhali-total-donation-amount").innerText =
+        raisedDonationForNoakhali;
+      alert("successfully added");
+    }
+    }else{
+      alert('invalid input')
+    }
+    
+  });
 
-//         })
+document
+  .getElementById("donate-for-feni")
+  .addEventListener("click", function () {
+    const accountBalance = document.getElementById("account-balance").innerText;
+    let balance = parseFloat(accountBalance);
+
+    const feniTotalDonationAmount = document.getElementById(
+      "feni-total-donation-amount"
+    ).innerText;
+    let feniTotalDonation = parseFloat(feniTotalDonationAmount);
+
+    const donatingAmountForFeni = getInputFieldValueById(
+      "input-money-for-feni"
+    );
+
+    if(!isNaN(donatingAmountForFeni)){
+      const donationAmount = parseFloat(donatingAmountForFeni);
+
+      if (donationAmount > balance) {
+        alert("You have insufficient balance");
+      } else {
+        const updatedBalance = balance - donationAmount;
+        document.getElementById("account-balance").innerText = updatedBalance;
+        const raisedDonationForFeni = feniTotalDonation + donationAmount;
+        document.getElementById("feni-total-donation-amount").innerText =
+          raisedDonationForFeni;
+        alert("successfully added");
+      }
+    }else{
+      alert('invalid input')
+    }
+   
+  });
+
+document
+  .getElementById("donate-for-quota-protest")
+  .addEventListener("click", function () {
+    const accountBalance = document.getElementById("account-balance").innerText;
+    let balance = parseFloat(accountBalance);
+
+    const quotaTotalDonationAmount = document.getElementById(
+      "quota-total-donation-amount"
+    ).innerText;
+    let quotaTotalDonation = parseFloat(quotaTotalDonationAmount);
+
+    const donatingAmountForQuota = getInputFieldValueById(
+      "input-money-for-quota"
+    );
+
+    if(!isNaN(donatingAmountForQuota)){
+      const donationAmount = parseFloat(donatingAmountForQuota);
+
+    if (donationAmount > balance) {
+      alert("You have insufficient balance");
+    } else {
+      const updatedBalance = balance - donationAmount;
+      document.getElementById("account-balance").innerText = updatedBalance;
+      const raisedDonationForQuota = quotaTotalDonation + donationAmount;
+      document.getElementById("quota-total-donation-amount").innerText =
+        raisedDonationForQuota;
+      alert("successfully added");
+    }
+    }else{
+      alert('invalid input')
+    }
+    
+  });
--- js/utilities.js
+++ js/utilities.js
@@ -1,6 +1,6 @@
 // console.log('utilities')
 
-function getInputFeldValueById (id){
+function getInputFieldValueById (id){
     const inputValue = document.getElementById(id).value;
     document.getElementById(id).value = "";
     return inputValue;
