b50bc71a56fd028c5bf3ca3fb71489af97ce9177
Joy Chowdhury <imjoychy69@gmail.com>
2024-09-25 01:16:29 +0600

    styled history section styled donation and history button background and stickyness. make almost whole website responsive for both mobile and large devices

--- js/utilities.js
+++ js/utilities.js
@@ -1,10 +1,12 @@
-// console.log('utilities')
+
 
 function getInputFieldValueById (id){
     const inputValue = document.getElementById(id).value;
     document.getElementById(id).value = "";
     return inputValue;
 }
+
+
 
 function showSectionById(id){
     document.getElementById('donation-section').classList.add('hidden');
@@ -14,31 +16,41 @@ function showSectionById(id){
 
 }
 
+
+
 function changeActiveButtonBackground(id){
     document.getElementById('btn-donation').style.background = '#FFFFFF';
     document.getElementById('btn-history').style.background = '#FFFFFF';
     document.getElementById(id).style.background = '#B4F461';
     document.getElementById(id).style.color = '#111111';
-
 }
+
+
 
 function generateMsg(msg){
     if(msg === 'noakhali'){
-        return `Taka is Donated for famine-2024 at Noakhali, Bangladesh`
+        return `Taka is Donated for Flood at Noakhali, Bangladesh`
     }else if(msg === 'feni'){
-        return `Taka is Donated for famine-2024 at Feni, Bangladesh`
+        return `Taka is Donated for Flood Relief in Feni,Bangladesh`
     }else{
-        return `Taka is Donated for famine-2024 at Quota protest, Bangladesh`
+        return `Taka is Donated for Aid for Injured in the Quota Movement,Bangladesh`
     }
 }
+
+
 
 function showDonationHistory(id, amount,text){
     const historySection = document.getElementById(id);
     const historyDiv = document.createElement('div');
+    historyDiv.classList.add('border-2')
+    historyDiv.classList.add('border-[#94a3b8]')
+    historyDiv.classList.add('rounded-lg')
+    historyDiv.classList.add('p-8')
+    historyDiv.classList.add('mb-8')
     historyDiv.innerHTML =  `
-        <h1 class="text-xl font-extrabold text-[#111111]">${amount} ${generateMsg(text)} </h1>
-        <p>${Date()}</p>
+        <h1 class="text-md font-bold text-[#111111] mb-2">${amount} ${generateMsg(text)} </h1>
+        <p class="text-[16px] text-[#111111] opacity-[0.7] " >Date : ${Date()}</p>
     `
-    historySection.appendChild(historyDiv)
+    historySection.appendChild(historyDiv);
 
 }
